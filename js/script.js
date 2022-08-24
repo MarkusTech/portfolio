e693de367ced0aab67221a684a7cac8b4b39b1cf
MarkusTech <55816864+MarkusTech@users.noreply.github.com>
2022-08-24 09:46:01 +0800

    portfolio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing",{
+    strings:[" ","Web Developer","Backend Developer","Full-stack Developer","System Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
